1805cd34c0413b3ad8332b3b043854c681faaacc
Yu-Chun Lin <count2841@gmail.com>
2024-02-25 21:21:04 +0800

    Add q_remove_head()
    The q_remove_head() function enables removal of elements from the
    head of a queue. It adheres to the "remove" concept by unlinking the
    element without deallocating memory. And It copies the removed
    element's value to a provided buffer (sp) with proper size constraints
    and null termination. Finally, it returns the removed element.

--- queue.c
+++ queue.c
@@ -71,6 +71,7 @@ bool q_insert_tail(struct list_head *head, char *s)
         free(newNode);
         return false;
     }
+    copyStr[len] = '\0';
     strncpy(copyStr, s, len);
     newNode->value = copyStr;
     list_add_tail(&newNode->list, head);
@@ -80,7 +81,18 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *rmElement = list_first_entry(head, element_t, list);
+    list_del(&rmElement->list);
+
+    if (sp) {
+        strncpy(sp, rmElement->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return rmElement;
 }
 
 /* Remove an element from tail of queue */
